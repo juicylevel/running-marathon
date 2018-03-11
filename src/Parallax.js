1606f420f880c4abd92e8bfbdf164ddfd27b8e46
juicylevel <rykov87yuri@gmail.com>
2018-03-11 17:26:10 +0300

    remove tabs

--- src/Parallax.js
+++ src/Parallax.js
@@ -1,31 +1,31 @@
 import $ from 'jquery';
 
 class Parallax {
-	constructor () {
-		this.init();
-	}
+    constructor () {
+        this.init();
+    }
 
-	updateLayer ($layer) {
-		const topDistance = window.pageYOffset;
-		const depth = $layer.attr('data-depth');
-	    const movement = -(topDistance * depth);
+    updateLayer ($layer) {
+        const topDistance = window.pageYOffset;
+        const depth = $layer.attr('data-depth');
+        const movement = -(topDistance * depth);
 
-	    $layer.css({
-	    	top: movement + 'px'
-	    })
-	}
-    
-	handleScroll (event) {
-		const me = this;
-		
-		$('.layer').each(function () {
-			me.updateLayer($(this));
-		})
-	}
+        $layer.css({
+            top: movement + 'px'
+        })  
+    }
 
-	init () {
-		$(window).on('scroll', this.handleScroll.bind(this));
-	}
+    handleScroll (event) {
+        const me = this;
+
+        $('.layer').each(function () {
+            me.updateLayer($(this));
+        })
+    }
+
+    init () {
+        $(window).on('scroll', this.handleScroll.bind(this));
+    }
 }
 
 export default Parallax;
