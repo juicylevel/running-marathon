7eebc0e18bd79762d1d65615d5543b9d04a19d99
Рыков Юрий Александрович <rykovya@pscb.ru>
2018-03-15 14:44:33 +0300

    script scrolling page control

--- src/Parallax.js
+++ src/Parallax.js
@@ -1,32 +1,86 @@
 import $ from 'jquery';
-import { TweenLite, Power1 } from 'gsap';
+import { TweenMax, Power1 } from 'gsap';
+import 'gsap/ScrollToPlugin';
 
 class Parallax {
     constructor () {
+        this.mousewheelHandler = this.mousewheelHandler.bind(this);
         this.init();
     }
 
-    updateLayer ($layer) {
-        const topDistance = window.pageYOffset;
-        const depth = $layer.attr('data-depth');
-        const movement = -(topDistance * depth);
+    // updateLayer ($layer) {
+    //     const topDistance = window.pageYOffset;
+    //     const depth = $layer.attr('data-depth');
+    //     const movement = -(topDistance * depth);
 
-        TweenLite.to($layer, 1.5, { 
-            top: movement, 
+    //     TweenLite.to($layer, 1.5, { 
+    //         top: movement, 
+    //         ease: Power1.easeOut
+    //     });  
+    // }
+
+    // handleScroll () {
+    //     const me = this;
+
+    //     $('.layer').each(function () {
+    //         me.updateLayer($(this));
+    //     });
+    // }
+
+    // updateLayer ($layer, delta) {
+        
+    // }
+
+    // updateLayers (delta) {
+    //     const me = this;
+
+    //     $('.layer').each(function () {
+    //         me.updateLayer($(this), delta);
+    //     });
+    // }
+
+    scrollPage (delta) {
+        const time = 0.8;
+        const distance = 150;
+        const $window = $(window);
+
+        TweenMax.to($window, time, { 
+            scrollTo: { 
+                y: $window.scrollTop() - (distance * delta)
+            }, 
             ease: Power1.easeOut
-        });  
-    }
-
-    handleScroll () {
-        const me = this;
-
-        $('.layer').each(function () {
-            me.updateLayer($(this));
         });
     }
 
+    mousewheelHandler (event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        }
+        event.returnValue = false;
+
+        let delta;
+
+        if (event.wheelDelta) {
+            delta = event.wheelDelta / 120;
+        } else if (event.detail) {
+            delta = -event.detail / 3;
+        } else {
+            delta = 0;
+        }
+
+        this.scrollPage(delta);
+    }
+
     init () {
-        $(window).on('scroll', this.handleScroll.bind(this)); 
+        // $(window).on('scroll', this.handleScroll.bind(this)); 
+        // $(window).on('mousewheel DOMMouseScroll', this.mousewheelHandler); 
+
+        if (window.addEventListener) {
+            window.addEventListener('DOMMouseScroll', this.mousewheelHandler, false);
+        }
+
+        window.onmousewheel = 
+        document.onmousewheel = this.mousewheelHandler;
     }
 }
 
